867ae6f497c42d8b9832aba4d1e25e6b8e2ad354
adubasava <anzhalikad@gmail.com>
2024-03-08 20:51:32 +0300

    Add password match validation

---
+++ script.js
@@ -0,0 +1,24 @@
+﻿addEventListener("submit", checkPassword);
+
+function checkPassword(event) {
+    event.preventDefault();
+    let password1 = document.querySelector('#pass1').value; 
+    let password2 = document.querySelector('#pass2').value; 
+    let message = document.querySelector('.message'); 
+    
+    if (password1 == '') {
+        message.textContent = "* Please, enter password!";
+        document.querySelector('#pass1').classList.add("error");
+    } else if (password2 == '') {
+        message.textContent = "* Please, enter confirmation!";        
+        document.querySelector('#pass2').classList.add("error");
+    } else if (password1 != password2) { 
+        message.textContent = "* Passwords do not match!";        
+        document.querySelector('#pass1').classList.add("error");       
+        document.querySelector('#pass2').classList.add("error");
+    } else {
+        document.querySelector('#pass1').classList.remove("error");       
+        document.querySelector('#pass2').classList.remove("error");
+        message.textContent = "";
+    }
+} 
